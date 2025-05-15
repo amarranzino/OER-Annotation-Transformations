926ad82136c9ddfd0610d515b12d1b5b62a50016
Ashley Marranzino <ashley.marranzino@noaa.gov>
2025-05-15 17:33:20 -0400

    Update DARC_SeaTube_Transformation.R
    added more to code

--- DARC_SeaTube_Transformation.R
+++ DARC_SeaTube_Transformation.R
@@ -22,3 +22,23 @@ annotation_import <- read_tsv(paste0(wd, "/", data_name, "_DARC_Annotations_Full
 #Review annotation import to make sure it looks okay
 str(annotation_import)
 view(annotation_import)
+
+#Create a dataframe with annotaor email addresses to reference later
+#NEED TO: figure out how to check that these are the only names in the CreatorName col
+emails <- data.frame (name = c("Putts, Meagan", "Hcarlson", "Bingo, Sarah", "Cunanan, Nikki", "Judah, Aaron"),
+                      email = c( "puttsmr@hawaii.edu", "hcarlson@hawaii.edu", "sarahr6@hawaii.edu", "tngutlay@hawaii.edu", "ajudah@hawaii.edu" ))
+
+#Create a column that has dive number 
+#This is different for Okeanos vs Nautilus dives
+
+annotation_clean <- as.data.frame(annotation_import) 
+
+if (annotation_clean$Vessel[1] == "Okeanos Explorer"){
+   annotation_clean <- annotation_clean |> 
+   mutate(DiveNumber = str_sub(Station, -2, -1))
+} else if (annotation_clean$Vessel[1] == "Nautilus"){
+   annotation_clean <- annotation_clean |> 
+     mutate(DiveNumber = str_sub(Station, -3, -1))
+}
+  
+
