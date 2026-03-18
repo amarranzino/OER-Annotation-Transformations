1d5fb48cb83646e6268ea3a603438a7ebae2d346
Ashley Marranzino <ashley.marranzino@noaa.gov>
2026-03-18 15:30:13 -0400

    Update DARC_SeaTube_Transformation.R

--- DARC_SeaTube_Transformation.R
+++ DARC_SeaTube_Transformation.R
@@ -20,20 +20,14 @@ if(!require('worrms'))install.packages('worrms'); library('worrms')
 
 #set standard name to refer to your data (e.g., the expedition number)
 
-data_name <- "EX2304"
+data_name <- "EX2301"
 
 #Read in the DARC TSV containing All annotations from expedition. Make sure the file name follows the convention Expedition number + "_DARC_Annotations_Full.tsv"
 
-annotation_import <- read_tsv(paste0(wd, "/DARC Annotations", data_name, "_DARC_Annotations_Full.tsv"))
+annotation_import <- read_tsv(paste0(wd, "/DARC Annotations/", data_name, "_DARC_Annotations_Full.tsv"))
 
 #Review annotation import to make sure it looks okay
 str(annotation_import)
-
-
-#Create a dataframe with annotator email addresses to reference later
-#NEED TO: figure out how to check that these are the only names in the CreatorName col
-# emails <- data.frame (name = c("Putts, Meagan", "hcarlson", "Bingo, Sarah", "Cunanan, Nikki", "Judah, Aaron"),
-#                       email = c( "meagan.putts@noaa.gov", "hcarlson@hawaii.edu", "sarahr6@hawaii.edu", "tngutlay@hawaii.edu", "ajudah@hawaii.edu" ))
 
 #Create a column that has dive number 
 #This is different for Okeanos vs Nautilus dives
